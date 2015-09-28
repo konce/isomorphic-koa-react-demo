01e8a47c301cb846bd5313116ac4a9fac83b77fe
kangxiaojun <kangxiaojun@zufangit.cn>
2015-09-28 15:00:03 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: "./page/index.jsx",
+    output: {
+        path: "./public",
+        filename: "index.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js/, loader: "babel-loader" }
+        ]
+    }
+};
